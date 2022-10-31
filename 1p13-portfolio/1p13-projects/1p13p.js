c5bd4b4b843560283651dda3350f0cacfe5694aa
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-10-30 20:21:23 -0400

    image upload + modal updates

--- 1p13-portfolio/1p13-projects/1p13p.js
+++ 1p13-portfolio/1p13-projects/1p13p.js
@@ -1,15 +1,18 @@
 var modal = document.getElementById("openpdf");
 var btn = document.getElementById("openpdfbutton");
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("p-proj-modal-close")[0];
 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
