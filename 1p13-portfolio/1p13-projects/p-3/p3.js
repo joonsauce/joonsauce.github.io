ee3e35c9dae9badf2db25f96dd4dcc0c609c2cf4
Joon <77248354+joonsauce@users.noreply.github.com>
2023-04-11 11:08:58 -0400

    moved p3 & p4 scripts to own file

---
+++ 1p13-portfolio/1p13-projects/p-3/p3.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#overview").fadeIn(1500);  
+    $("#figure1").delay(500).fadeIn(2000);
+    $("#skills").delay(1250).fadeIn(2000);
+    $("#figure2").delay(2000).fadeIn(2000);
+    $("#design").delay(2500).fadeIn(2000);
+    $("#figure3").delay(3000).fadeIn(2000);
+    $("#admin").delay(3500).fadeIn(2000);
+    $("#figure4").delay(4000).fadeIn(2000);
+    $("#learn").delay(4500).fadeIn(2000);
+    $("#figure5").delay(5000).fadeIn(2000);
+    $("#thoughts").delay(5500).fadeIn(2000);
+});
---
+++ 1p13-portfolio/1p13-projects/p-4/p4.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#overview").fadeIn(1500);  
+    $("#figure1").delay(500).fadeIn(2000);
+    $("#img1").delay(500).animate({width: "30vw"},2000);
+    $("#skills").delay(1250).fadeIn(2000);
+    $("#figure2").delay(2000).fadeIn(2000);
+    $("#img2").delay(2000).animate({width: "30vw"},2000);
+    $("#design").delay(2500).fadeIn(2000);
+    $("#figure3").delay(3000).fadeIn(2000);
+    $("#img3").delay(3000).animate({width: "30vw"},2000);
+    $("#admin").delay(3500).fadeIn(2000);
+    $("#figure4").delay(4000).fadeIn(2000);
+    $("#img4").delay(4000).animate({width: "30vw"},2000);
+    $("#learn").delay(4500).fadeIn(2000);
+    $("#figure5").delay(5000).fadeIn(2000);
+    $("#img5").delay(5000).animate({width: "30vw"},2000);
+    $("#thoughts").delay(5500).fadeIn(2000);
+});
