b17079af31d6271839973847fa72dabc48d5e06f
Joon <77248354+joonsauce@users.noreply.github.com>
2023-04-11 12:52:32 -0400

    update landing page & styles

---
+++ 1p13-portfolio/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#l1").fadeIn(2000);  
+    $("#l2").delay(0).fadeIn(2500);
+    $("#latest-project").delay(500).fadeIn(2500);
+    $("#about").delay(1000).fadeIn(2000);
+    $("#joon").delay(1500).fadeIn(2500);
+    $("#contact").delay(2000).fadeIn(2000);
+});
