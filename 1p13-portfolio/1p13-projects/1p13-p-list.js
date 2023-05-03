c25cfd21f9e508af690801c5bf3c3875fefaf93c
joonsauce <77248354+joonsauce@users.noreply.github.com>
2023-05-03 19:39:40 -0400

    moving 1p13 portfolio to own repo

--- 1p13-portfolio/1p13-projects/1p13-p-list.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#p1").fadeIn(2000);
-    $("#p2").delay(250).fadeIn(2000);
-    $("#p3").delay(500).fadeIn(2000);
-    $("#p4").delay(750).fadeIn(2000);
-});
--- 1p13-portfolio/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#l1").fadeIn(2000);  
-    $("#l2").delay(0).fadeIn(2500);
-    $("#latest-project").delay(500).fadeIn(2500);
-    $("#about").delay(1000).fadeIn(2000);
-    $("#joon").delay(1500).fadeIn(2500);
-    $("#contact").delay(2000).fadeIn(2000);
-});
